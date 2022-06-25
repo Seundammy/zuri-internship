e182e6fb1c8bfbf896c3c886cd511121e6663ffe
oluwaseun akenlomo <52778101+Seundammy@users.noreply.github.com>
2022-06-25 17:52:05 +0000

    modified index.html file

---
+++ new/script.js
@@ -0,0 +1,7 @@
+let h1 = document.querySelector('.user-details')
+const surname = "Akenlomo";
+const firstname = "Oluwaseun";
+const height = "5.8ft";
+const country = "Nigeria";
+h1.textContent = `My name is ${surname} ${firstname} I'm ${height} tall. I'm a citizen of ${country}`;
+
